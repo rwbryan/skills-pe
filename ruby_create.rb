f5e16d17ad70f9e85fe581244cd204bec7c88c34
rwbryan <rwbryan@gmail.com>
2017-01-17 00:25:17 -0500

    Create ruby_create.rb

---
+++ ruby_create.rb
@@ -0,0 +1,11 @@
+# Ruby file that was attempted to create Droplet servers
+# NOTE: This ruby files has not been tested
+
+#!/usr/bin/ruby
+
+require 'droplet_kit'
+token='TOKEN-NUMBER'
+client = DropletKit::Client.new(access_token: token)
+
+droplet = DropletKit::Droplet.new(name: 'RBserver', region: 'nyc1', size: '512m', image: 'CentOS-7-3-1611-x64')
+client.droplets.create(droplet)
